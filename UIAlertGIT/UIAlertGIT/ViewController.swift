a81c0f8d2aa94693ceb0298a66f4ca4d7a8f0350
VitaliiKryvoshapkaDev <v.kryvoshapka@icloud.com>
2021-10-24 00:47:30 +0300

    Added Alert

--- UIAlertGIT/UIAlertGIT/ViewController.swift
+++ UIAlertGIT/UIAlertGIT/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Add Action Button
+    @IBAction func actionShowAlert() {
+        let alert = UIAlertController(title: "Photo Acces", message: "Do you allow to use yout photos", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet() {
+        
+        
+    }
 }
 
