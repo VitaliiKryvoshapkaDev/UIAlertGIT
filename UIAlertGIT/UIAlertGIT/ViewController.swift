1d38cd5c060cb4a5e90fb99655cd949f16e7ad89
VitaliiKryvoshapkaDev <v.kryvoshapka@icloud.com>
2021-10-24 00:13:27 +0300

    Added project

---
+++ UIAlertGIT/UIAlertGIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIAlertGIT
+//
+//  Created by Vitalii Kryvoshapka on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
