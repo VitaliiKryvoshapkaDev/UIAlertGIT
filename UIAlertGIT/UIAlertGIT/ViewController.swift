f8d2fcc4375cc00043070c2caff3c0a7f993fe33
VitaliiKryvoshapkaDev <v.kryvoshapka@icloud.com>
2021-10-24 01:11:24 +0300

    Added AlertSheet

--- UIAlertGIT/UIAlertGIT/ViewController.swift
+++ UIAlertGIT/UIAlertGIT/ViewController.swift
@@ -27,8 +27,38 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    //Add Action Sheet Button
     @IBAction func showActionSheet() {
         
+        //Add Enum of Films
+        enum Films:String{
+            case film1 = "Amazing Spider Man"
+            case film2 = "Upon time in Hollywood"
+        }
+        
+        let alert = UIAlertController(title: nil, message: "Choose Film!", preferredStyle: .actionSheet)
+        
+        //Add Handler
+        let handler:(_ type: Films) ->  (UIAlertAction) -> Void = {
+            type in
+            //do something
+            return {
+                //do something
+                action in
+                print(type.rawValue)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let film1 = UIAlertAction(title: Films.film1.rawValue, style: .default, handler: handler(.film1))
+        alert.addAction(film1)
+        
+        let film2 = UIAlertAction(title: Films.film2.rawValue, style: .default, handler: handler(.film2))
+        alert.addAction(film2)
+        
+        present(alert, animated: true, completion: nil)
         
     }
 }
