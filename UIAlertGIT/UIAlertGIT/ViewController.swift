7995180eec055b3ded546f56f722c7916d350ad3
VitaliiKryvoshapkaDev <v.kryvoshapka@icloud.com>
2021-10-24 15:37:32 +0300

    Added Film 3

--- UIAlertGIT/UIAlertGIT/ViewController.swift
+++ UIAlertGIT/UIAlertGIT/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
         enum Films:String{
             case film1 = "Amazing Spider Man"
             case film2 = "Upon time in Hollywood"
+            case film3 = "Kill Bill"
         }
         
         let alert = UIAlertController(title: nil, message: "Choose Film!", preferredStyle: .actionSheet)
@@ -57,6 +58,9 @@ class ViewController: UIViewController {
         
         let film2 = UIAlertAction(title: Films.film2.rawValue, style: .default, handler: handler(.film2))
         alert.addAction(film2)
+        
+        let film3 = UIAlertAction(title: Films.film3.rawValue, style: .default, handler: handler(.film3))
+        alert.addAction(film3)
         
         present(alert, animated: true, completion: nil)
         
